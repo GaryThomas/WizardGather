13b55896a7254eefcab79f32831ffe9fa211c0bf
Gary Thomas <gary@mlbassoc.com>
2018-05-22 04:43:28 +0100

    Import various packages.  Experiment with 2D physics

---
+++ Assets/Workspace/Obstacle.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		Debug.Log (gameObject.name + " Collided with " + other.gameObject.name);
+	}
+}
---
+++ Assets/Workspace/Test2D.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2D : MonoBehaviour
+{
+	Rigidbody2D _rb;
+
+	void Awake ()
+	{
+		_rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void Start ()
+	{
+		_rb.velocity = new Vector2 (1.5f, 2.0f);
+	}
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		Debug.Log (gameObject.name + " Collided with " + other.gameObject.name);
+	}
+}
